5381e5ac0c430f582bc99eddaf0896ff25b105fd
Devon Friend <69879126+DFriend01@users.noreply.github.com>
2021-10-04 13:11:04 -0700

    Added ControllerOutputRefiner class with saturation function (#7)
    Task Link: https://ubcsailbot.atlassian.net/browse/BSC-67

---
+++ python/controller_output_refiner.py
@@ -0,0 +1,39 @@
+class ControllerOutputRefiner:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def saturate(inputSignal, upperBound, lowerBound):
+        """
+        This function saturates an input signal between an upper bound and a
+        lower bound. If the input signal is greater than the upper bound, then
+        the upper bound is returned. If the input signal is smaller than the
+        lower bound, then the lower bound is returned. Otherwise, the input
+        signal is return unmodified.
+
+        Paramters
+        ---------
+        inputSignal : float
+            A float representing the input signal.
+
+        upperBound : float
+            A float representing the upper bound for the saturated signal.
+
+        lowerBound : float
+            A float representing the lower bound for the saturated signal.
+
+        Returns
+        -------
+        float
+            The saturated signal bounded between the upper and lower bound.
+        """
+
+        # Check for bad inputs
+        assert(upperBound >= lowerBound)
+
+        if (lowerBound <= inputSignal <= upperBound):
+            return inputSignal
+        elif (inputSignal > upperBound):
+            return upperBound
+        else:
+            return lowerBound
