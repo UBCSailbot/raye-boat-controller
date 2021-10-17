c77da9fed4ae42ee505dec5a741cb03a857a25bb
DFriend01 <devon.fri.2000@gmail.com>
2021-10-17 15:21:20 -0700

    Simplified feedback gain function

--- python/rudder_controller.py
+++ python/rudder_controller.py
@@ -11,12 +11,13 @@ class RudderController(HeadingController):
     # How feed back gain is calculated is taken from equation 5.2 on page 25 of this paper:
     # https://core.ac.uk/download/pdf/79618904.pdf
     def get_feed_back_gain(heading_error):
-        if heading_error > 2*math.pi or heading_error < -2*math.pi:
+
+        if (abs(heading_error) > 2 * math.pi):
             raise ValueError("heading_error must be between -2pi and 2pi")
-        while(heading_error >= 2*math.pi):
-            heading_error -= 2*math.pi
-        while(heading_error <= -2*math.pi):
-            heading_error += 2*math.pi
+
+        # Bound the heading error between -2pi and 2pi
+        if (abs(heading_error) >= 2 * math.pi):
+            heading_error %= 2 * math.pi
 
         return sailbot_constants.KP / (1 + sailbot_constants.CP * abs(heading_error))
 
