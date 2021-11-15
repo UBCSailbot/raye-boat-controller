64d0a32f67ef1bd7737885595dc2b4fe1672ef10
Pidud <TsuchitoriJoel@gmail.com>
2021-11-14 23:43:04 -0800

    Refactored Jibe Controller Name

--- test/test_JibeController.py
+++ test/test_JibeController.py
@@ -9,7 +9,7 @@ import rostest
 import unittest
 import math
 import sailbot_constants
-from jibe_only_rudder_controller import JibeOnlySailController
+from jibe_only_rudder_controller import JibeOnlyRudderController
 
 # Do something with local_imports to avoid lint errors
 local_imports.printMessage()
@@ -18,28 +18,28 @@ local_imports.printMessage()
 class Test_JibeController(unittest.TestCase):
 
     def test_getJibeDirection_normalCase(self):
-        self.assertEqual(JibeOnlySailController.get_jibe_controller_direction(math.pi /2, - math.pi / 2, 0), -1)
+        self.assertEqual(JibeOnlyRudderController.get_jibe_controller_direction(math.pi /2, - math.pi / 2, 0), -1)
 
     def test_getJibeError_normalCase(self):
-        self.assertAlmostEqual(JibeOnlySailController.get_jibe_controller_error(math.pi /2, - math.pi / 2, -1), -math.pi)
+        self.assertAlmostEqual(JibeOnlyRudderController.get_jibe_controller_error(math.pi /2, - math.pi / 2, -1), -math.pi)
 
     def test_getJibeDirection_normalCaseInverse(self):
-        self.assertEqual(JibeOnlySailController.get_jibe_controller_direction(math.pi /2, - math.pi / 2, math.pi), 1)
+        self.assertEqual(JibeOnlyRudderController.get_jibe_controller_direction(math.pi /2, - math.pi / 2, math.pi), 1)
 
     def test_getJibeError_normalCaseInverse(self):
-        self.assertAlmostEqual(JibeOnlySailController.get_jibe_controller_error(math.pi /2, - math.pi / 2, 1), math.pi)
+        self.assertAlmostEqual(JibeOnlyRudderController.get_jibe_controller_error(math.pi /2, - math.pi / 2, 1), math.pi)
 
     def test_getJibeDirection_smallAngle(self):
-        self.assertEqual(JibeOnlySailController.get_jibe_controller_direction(math.pi /100, - math.pi / 100, math.pi), 1)
+        self.assertEqual(JibeOnlyRudderController.get_jibe_controller_direction(math.pi /100, - math.pi / 100, math.pi), 1)
 
     def test_getJibeError_smallAngle(self):
-        self.assertAlmostEqual(JibeOnlySailController.get_jibe_controller_error(math.pi /100, - math.pi / 100, 1), (99/100) * 2 * math.pi)
+        self.assertAlmostEqual(JibeOnlyRudderController.get_jibe_controller_error(math.pi /100, - math.pi / 100, 1), (99/100) * 2 * math.pi)
 
     def test_getJibeDirection_wrap(self):
-        self.assertEqual(JibeOnlySailController.get_jibe_controller_direction(math.pi /2 + 2 * math.pi, - math.pi / 2 + 2 * math.pi, 0 + 2 * math.pi), -1)
+        self.assertEqual(JibeOnlyRudderController.get_jibe_controller_direction(math.pi /2 + 2 * math.pi, - math.pi / 2 + 2 * math.pi, 0 + 2 * math.pi), -1)
 
     def test_getJibeError_wrap(self):
-        self.assertAlmostEqual(JibeOnlySailController.get_jibe_controller_error(math.pi /2 + 2 * math.pi, - math.pi / 2 + 2 * math.pi, -1), -math.pi)
+        self.assertAlmostEqual(JibeOnlyRudderController.get_jibe_controller_error(math.pi /2 + 2 * math.pi, - math.pi / 2 + 2 * math.pi, -1), -math.pi)
 if __name__ == "__main__":
     rostest.rosrun("boat_controller", "Test_JibeController",
                     Test_JibeController)
