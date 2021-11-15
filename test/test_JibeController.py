cb4b070bb4dd13d3c99771cde090840ae8d3c698
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2021-11-14 23:03:42 -0800

    fixed merge error

--- test/test_JibeController.py
+++ test/test_JibeController.py
@@ -41,9 +41,5 @@ class Test_JibeController(unittest.TestCase):
     def test_getJibeError_wrap(self):
         self.assertAlmostEqual(JibeOnlySailController.get_jibe_controller_error(math.pi /2 + 2 * math.pi, - math.pi / 2 + 2 * math.pi, -1), -math.pi)
 if __name__ == "__main__":
-<<<<<<< HEAD
-    rostest.rosrun("jibe_only_rudder_controller", "Test_JibeController",
-=======
     rostest.rosrun("boat_controller", "Test_JibeController",
->>>>>>> bab88b2d6551ce491528d9a0173c6835a18ccbcc
                     Test_JibeController)
