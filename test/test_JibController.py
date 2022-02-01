1ac0a8e6deb21e68a21267600842dbeb357d37ab
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2022-01-31 17:39:55 -0800

    Fixed file prefix on Jib Controller

--- test/test_JibController.py
+++ test/test_JibController.py
@@ -66,4 +66,4 @@ class Test_JibController(unittest.TestCase):
 
 if __name__ == "__main__":
     rostest.rosrun("boat_controller", "Test_JibController",
-                   Test_JibController)
+                  Test_JibController)
