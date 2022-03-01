900090163e1da20e495386ef69919c71355f88c1
Maziyar Dowlatabadibazaz <79039790+maziyardowlat@users.noreply.github.com>
2022-02-28 18:38:28 -0700

    Add files via upload
    the low power baot controller

---
+++ python/boat_controller_low_power.py
@@ -0,0 +1,16 @@
+import sailbot_constants
+import math
+
+class BoatLowPower:
+    def __init__(self):
+        pass
+
+    @staticmethod
+
+    def get_low_power(x):
+        Low = 10
+        if(x < Low):
+            return True
+        else:
+            return False
+
