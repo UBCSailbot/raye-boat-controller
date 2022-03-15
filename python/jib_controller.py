4e2409088bc68bdc1537b01d2f416e3c6db7b77b
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2022-03-14 17:51:28 -0700

    Updated Jib Controller

--- python/jib_controller.py
+++ python/jib_controller.py
@@ -15,9 +15,19 @@ class JibController:
        # bound angle to be between -pi and pi based on this post: https://stackoverflow.com/a/2321125
         bounded_angle = math.atan2(
             math.sin(apparent_wind_angle_rad), math.cos(apparent_wind_angle_rad))
-        return (
-            sailbot_constants.JIB_CONTROLLER_MAX_SAIL_ANGLE
-            / (-math.pi)
-            * abs(bounded_angle)
-            + sailbot_constants.JIB_CONTROLLER_MAX_SAIL_ANGLE
-        )
+            
+      #Generate Slope Value based on X1, X2, and the Max Sail angle
+        k_s =  (-sailbot_constants.SAIL_CONTROLLER_MAX_SAIL_ANGLE ) / (sailbot_constants.X2_JIB - sailbot_constants.X1_JIB )
+
+        #Generate Linear function from slope and intercept
+        jib_angle = (k_s * abs(bounded_angle)) - (k_s * sailbot_constants.X2_JIB)  
+
+        #Clamp Linear Function
+        min = 0
+        max = sailbot_constants.SAIL_CONTROLLER_MAX_SAIL_ANGLE
+
+        if(jib_angle > max):
+            return max
+        if(jib_angle < min):
+            return min
+        else: return jib_angle
