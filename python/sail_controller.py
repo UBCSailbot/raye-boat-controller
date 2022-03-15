83b57d1ba1b8294ad9aeefbd7c9b5e3be4191ef6
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2022-03-14 17:18:12 -0700

    Prototype Adjustable Sailcontroller

--- python/sail_controller.py
+++ python/sail_controller.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 import sailbot_constants
 import math
 
@@ -15,9 +16,17 @@ class SailController:
         # bound angle to be between -pi and pi based on this post: https://stackoverflow.com/a/2321125
         bounded_angle = math.atan2(
             math.sin(apparent_wind_angle_rad), math.cos(apparent_wind_angle_rad))
-        return (
-            sailbot_constants.SAIL_CONTROLLER_MAX_SAIL_ANGLE
-            / (-math.pi)
-            * abs(bounded_angle)
-            + sailbot_constants.SAIL_CONTROLLER_MAX_SAIL_ANGLE
-        )
+
+        k_s =  (-sailbot_constants.SAIL_CONTROLLER_MAX_SAIL_ANGLE * sailbot_constants.X2_SAIL
+        ) / (sailbot_constants.X2_SAIL - sailbot_constants.X1_SAIL )
+        
+        sail_angle = (k_s * bounded_angle) - (k_s * sailbot_constants.X2_SAIL) 
+        + sailbot_constants.SAIL_CONTROLLER_MAX_SAIL_ANGLE
+        return Self.clamp(sail_angle, 0, sailbot_constants.SAIL_CONTROLLER_MAX_SAIL_ANGLE)
+    
+    def clamp(n, min, max):
+        if(n > max):
+            return max
+        if(n < min):
+            return min
+        else: return n
