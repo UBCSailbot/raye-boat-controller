d72db8198f8c90ddc3a366fe7cc5ba4f1f9b4968
DFriend01 <devon.fri.2000@gmail.com>
2022-05-14 14:08:28 -0700

    Reading from filtered sensor data and GPS data

--- test/mock_desiredHeadingAndSensors.py
+++ test/mock_desiredHeadingAndSensors.py
@@ -2,7 +2,7 @@
 
 import rospy
 import random
-from sailbot_msg.msg import heading, Sensors
+from sailbot_msg.msg import heading, windSensor, GPS
 ROSPY_RATE = 10  # hertz
 
 
@@ -10,53 +10,22 @@ def talker():
     heading_degrees_pub = rospy.Publisher(
         "/desired_heading_degrees", heading, queue_size=10
     )
-    sensors_pub = rospy.Publisher("/sensors", Sensors, queue_size=10)
+    windSensor_pub = rospy.Publisher("/windSensor", windSensor, queue_size=10)
+    gps_pub = rospy.Publisher("/GPS", GPS, queue_size=10)
 
     rospy.init_node("talker", anonymous=True)
     rate = rospy.Rate(ROSPY_RATE)
-    timestamp = "0"
+
     while not rospy.is_shutdown():
         current_heading = random.uniform(0.0, 45.0)
         desired_heading = random.uniform(0.0, 45.0)
         groundspeed = random.uniform(0.0, 2.0)
         windangle = int(random.uniform(0.0, 180.0))
-        timestamp = str(int(timestamp) + 1)
+
         heading_degrees_pub.publish(heading(desired_heading))
-        sensors_pub.publish(Sensors(
-            0,
-            0.0,
-            windangle,
-            0.0,
-            0,
-            0.0,
-            0,
-            "",
-            0.0,
-            0.0,
-            groundspeed,
-            0.0,
-            current_heading,
-            0.0,
-            True,
-            "0",
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            True,
-            0,
-            0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0,
-            0.0)
-        )
+        windSensor_pub.publish(windSensor(windangle, 1.))
+        gps_pub.publish(GPS(.0, .0, current_heading, groundspeed))
+
         rate.sleep()
 
 
